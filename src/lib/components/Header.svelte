5e0040feb76cde7a086f99c58d9e2b86d9b9928b
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-06-07 23:09:24 +0100

    update header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,31 @@
-<h1>P<a href="/player/mcpqndq">i</a>tMart</h1>
+<script>
+	import CenteredDiv from "$lib/components/page/CenteredDiv.svelte";
+</script>
 
-Search player
-
-<form method="GET" action="/player/">
-	<label>
-		<input name="tag" type="tag" placeholder="Enter username or UUID" />
-	</label>
-</form>
+<CenteredDiv>
+	<div>
+		<div>
+			<h1>P<a href="/player/mcpqndq">i</a>tMart</h1>
+		</div>
+		<div>
+			<span>Search player:</span>
+			<form method="GET" action="/player/">
+				<label>
+					<input name="tag" type="tag" placeholder="Enter username or UUID" />
+				</label>
+			</form>
+		</div>
+	</div>
+</CenteredDiv>
 
 <style>
 	h1 {
 		font-size: 64px;
+		margin: 8px;
+	}
+
+	form {
+		margin: 8px;
 	}
 
 	a {
