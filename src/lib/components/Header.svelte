73ba855984c841cb5243bb9cd6bf4c61bc6f1c0e
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-06-08 10:52:56 +0100

    update header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,14 @@
 <script>
 	import CenteredDiv from "$lib/components/page/CenteredDiv.svelte";
+
+	import { goto } from "$app/navigation";
+
+	let tag = "";
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		goto(`/player/${tag}`);
+	}
 </script>
 
 <CenteredDiv>
@@ -9,10 +18,11 @@
 		</div>
 		<div>
 			<span>Search player:</span>
-			<form method="GET" action="/player/">
+			<form on:submit={handleSubmit}>
 				<label>
-					<input name="tag" type="tag" placeholder="Enter username or UUID" />
+					<input bind:value={tag} type="text" placeholder="Enter username or UUID" />
 				</label>
+				<button type="submit">Search</button>
 			</form>
 		</div>
 	</div>
@@ -30,5 +40,24 @@
 
 	a {
 		text-decoration: none;
+	}
+
+	input {
+		width: 256px;
+		height: 32px;
+		font-size: 16px;
+		background-color: #999;
+		outline: 2px #111 solid;
+		border: none;
+	}
+
+	input:focus {
+		outline: 1px #999 solid;
+	}
+
+	::placeholder {
+		color: #333;
+		opacity: 1;
+		text-align: center;
 	}
 </style>
