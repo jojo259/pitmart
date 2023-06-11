2ed0d106f9e61344abbbafc4fba41e5d481f6e16
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-06-11 13:26:22 +0100

    fix and organize upgrades card code

--- src/lib/components/page/UpgradesCard.svelte
+++ src/lib/components/page/UpgradesCard.svelte
@@ -9,21 +9,36 @@
 	export let player: Player;
 
 	let perksData: PerksData = pitMaster.Pit.Perks;
-	let perksFakeInventory: Item[] = [];
+	let perksFakeInventory: (Item | null)[] = [];
 	$: {
 		perksFakeInventory = [];
 		player.upgrades.perks.forEach((perkKey: string) => {
-			const perkData = perksData[perkKey];
-			const itemData = perkData.Item;
-			let perkItem = {
-				id: perkData.Item.Id,
-				dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
-				count: 1,
-				name: perkData.Name,
-				lore: perkData.Description,
-				color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
-			} as Item
-			perksFakeInventory.push(perkItem);
+			if (perkKey) {
+				const perkData = perksData[perkKey];
+				if (perkData) {
+					const itemData = perkData.Item;
+					if (itemData) {
+						let perkItem = {
+							id: perkData.Item.Id,
+							dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
+							count: 1,
+							name: perkData.Name,
+							lore: perkData.Description,
+							color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
+						} as Item
+						perksFakeInventory.push(perkItem);
+					}
+					else {
+						console.error(`no perk item data for ${perkKey}`);
+					}
+				}
+				else {
+					console.error(`no perk data for ${perkKey}`);
+				}
+			}
+			else{
+				perksFakeInventory.push(null);
+			}
 		});
 	}
 
@@ -34,18 +49,28 @@
 		player.upgrades.killstreaks.forEach((killstreakKey: string) => {
 			if (killstreakKey) {
 				const killstreakData = killstreaksData[killstreakKey];
-				const itemData = killstreakData.Item;
-				let killstreakItem = {
-					id: killstreakData.Item.Id,
-					dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
-					count: 1,
-					name: killstreakData.Name,
-					lore: killstreakData.Description,
-					color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
-				} as Item;
-				killstreaksFakeInventory.push(killstreakItem);
+				if (killstreakData) {
+					const itemData = killstreakData.Item;
+					if (itemData) {
+						let killstreakItem = {
+							id: killstreakData.Item.Id,
+							dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
+							count: 1,
+							name: killstreakData.Name,
+							lore: killstreakData.Description,
+							color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
+						} as Item;
+						killstreaksFakeInventory.push(killstreakItem);
+					}
+					else {
+						console.error(`no killstreak item data for ${killstreakKey}`);
+					}
+				}
+				else {
+					console.error(`no killstreak data for ${killstreakKey}`);
+				}
 			}
-			else {
+			else{
 				killstreaksFakeInventory.push(null);
 			}
 		});
@@ -57,28 +82,37 @@
 		passivesFakeInventory = [];
 		for (let [playerPassiveKey, playerPassiveLevel] of Object.entries(player.upgrades.passives)) {
 			const passiveData = passivesData[playerPassiveKey];
-			const itemData = passiveData.Item;
+			if (passiveData) {
+				const itemData = passiveData.Item;
+				if (itemData) {
+					let passiveItem = {
+						id: itemData.Id,
+						dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
+						count: playerPassiveLevel,
+						name: passiveData.Name ? passiveData.Name : null,
+						lore: passiveData.Description ? passiveData.Description : null,
+						color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
+					} as Item;
 
-			let passiveItem = {
-				id: itemData.Id,
-				dataVal: typeof itemData.Meta === 'number' ? itemData.Meta : null,
-				count: playerPassiveLevel,
-				name: passiveData.Name ? passiveData.Name : null,
-				lore: passiveData.Description ? passiveData.Description : null,
-				color: typeof itemData.Meta === 'string' ? itemData.Meta : null,
-			} as Item;
+					if (playerPassiveLevel > 0) {
+						passiveItem.name = `${playerPassiveLevel > 1 ? '§9' : '§0'}${passiveItem.name} ${romanize(playerPassiveLevel)}`;
+					} else {
+						passiveItem.name = "§c" + passiveItem.name;
+					}
 
-			if (playerPassiveLevel > 0) {
-				passiveItem.name = `${playerPassiveLevel > 1 ? '§9' : '§0'}${passiveItem.name} ${romanize(playerPassiveLevel)}`;
-			} else {
-				passiveItem.name = "§c" + passiveItem.name;
+					if (passiveItem.lore) {
+						passiveItem.lore = passiveItem.lore.map(line => line.replace('$', Math.max(playerPassiveLevel, 1).toString()));
+					}
+
+					passivesFakeInventory.push(passiveItem);
+				}
+				else {
+					console.error(`no passives item data for ${playerPassiveKey}`);
+				}
 			}
-
-			if (passiveItem.lore) {
-				passiveItem.lore = passiveItem.lore.map(line => line.replace('$', Math.max(playerPassiveLevel, 1).toString()));
+			else {
+				console.error(`no passive data for ${playerPassiveKey}`);
 			}
-
-			passivesFakeInventory.push(passiveItem);
 		}
 	}
 
