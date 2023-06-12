df760cef610d5d59cc318b4f28a8d30b8ea32d21
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-06-12 19:28:51 +0100

    remove discord oauth guilds scope

--- src/routes/api/discord/auth/+server.ts
+++ src/routes/api/discord/auth/+server.ts
@@ -3,7 +3,7 @@ import { json } from "@sveltejs/kit";
 
 let callbackUrl = hostDomain + "/api/discord/callback";
 
-const discordEndpoint = `https://discord.com/api/oauth2/authorize?client_id=${discordClientId}&redirect_uri=${encodeURIComponent(hostDomain)}%2Fapi%2Fdiscord%2Fcallback&response_type=code&scope=identify%20guilds`;
+const discordEndpoint = `https://discord.com/api/oauth2/authorize?client_id=${discordClientId}&redirect_uri=${encodeURIComponent(hostDomain)}%2Fapi%2Fdiscord%2Fcallback&response_type=code&scope=identify`;
 
 export async function GET() {
 	return new Response(null, {
