bdd489d2a195d82e7cf2c3ae5001414021ced4a7
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-06-29 09:51:29 +0100

    add listings search list component & improve listing search api route

---
+++ src/lib/components/page/ListingsSearchList.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import ListingComponent from "$lib/components/page/Listing.svelte";
+	import type { Listing } from "$lib/types";
+
+	export let queryParams = "";
+
+	$: listingsPromise = (async function getListings(): Promise<Listing[] | null> {
+		let req = await fetch(`/api/listing/search?${queryParams}`);
+		let resp = await req.json();
+		if ("listings" in resp) {
+			let listings: Listing[] = resp.listings;
+			return listings;
+		}
+		return null;
+	})();
+</script>
+
+{#await listingsPromise}
+	listings {queryParams}
+{:then listings}
+	{#if listings}
+		{#each listings as listing}
+			<div>
+				<ListingComponent listing={listing} />
+			</div>
+		{/each}
+	{/if}
+{:catch error}
+	{error}
+{/await}
+
+<style>
+	div {
+		display: inline-block;
+	}
+</style>
--- src/routes/api/listing/search/+server.ts
+++ src/routes/api/listing/search/+server.ts
@@ -4,15 +4,68 @@ import { apiGetPlayer } from "$lib/serverutil";
 import { collections } from "$lib/modules/database";
 import type { Listing } from "$lib/types";
 
-export async function GET(req) {
+export async function GET({url}) {
 	if (!collections.listings) {
 		console.log("listings collection not defined");
 		return json({success: false, message: "database not initialized"});
 	}
 
-	let listingDocs: any = await collections.listings.find({}).toArray();
+	let dbQueryAnds: any = [];
+
+	/*
+
+		QUERY PARAMS FORMAT:
+			query parameter key: the document field to check
+			query parameter value: operation char followed by the operation value
+			operation char potential values:
+				s: string, checks for equality
+				e: number, checks for equality
+				g: number, checks for greater than
+				l: number, checks for less than
+			operation value: the value that is checked against
+			enchants are a special case which will be implemented later
+			examples:
+				item.id=e346 checks for item.id being equal to 346
+				item.lives=g5 checks for item.lives being greater than 5
+				item.maxLives=l10 checks for item.maxLives being less than 10
+				currency=svile checks for the listing's currency being equal to the string "vile"
+
+	*/
+
+	for (const [key, value] of url.searchParams.entries()) {
+		console.log(`${key}: ${value}`);
+		let operation = "$eq";
+		let operationChar = value.charAt(0);
+		let operationVal: string | number = value.slice(1);
+		if (["e", "s"].includes(operationChar)) {
+			operation = "$eq";
+		}
+		else if (operationChar == "g") {
+			operation = "$gt";
+		}
+		else if (operationChar == "l") {
+			operation = "$lt";
+		}
+		else {
+			console.log(`listing search param unknown last char: ${operationChar}`);
+			return json({success: false, message: "unknown query param operation char"});
+		}
+		if (["e", "g", "l"].includes(operationChar)) {
+			operationVal = parseInt(operationVal);
+		}
+		dbQueryAnds.push({[key]: {[operation]: operationVal}})
+	}
+
+	let dbQuery = {};
+	if (dbQueryAnds.length > 0) {
+		dbQuery = {$and: dbQueryAnds};
+	}
+
+	console.log(`db query: ${JSON.stringify(dbQuery)}`)
+
+	let listingDocs: any = await collections.listings.find(dbQuery).toArray();
 	if (!listingDocs) {
-		return json({success: false});
+		return json({success: true, listings: []});
 	}
 
 	let listings: Listing[] = listingDocs;
