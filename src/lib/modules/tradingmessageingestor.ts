9952fa0f061d2b80ef8d92ca7f5dd0219e43fd9d
jojo259 <95320642+jojo259@users.noreply.github.com>
2023-11-09 21:04:08 +0800

    improve trade message parser

--- src/lib/modules/tradingmessageingestor.ts
+++ src/lib/modules/tradingmessageingestor.ts
@@ -8,6 +8,7 @@ export function ingestTradingMessage(messageContent: string, messageAuthorId: st
 
 	// trash parser incoming
 
+	let parsedAnyInfoAboutItem = false;
 	let sellingFlag = false;
 	let buyingFlag = false;
 
@@ -15,8 +16,6 @@ export function ingestTradingMessage(messageContent: string, messageAuthorId: st
 
 	for (let line of messageLines) {
 		console.log(`at line: ${line}`);
-
-		let currentParam = "";
 
 		let listing: Listing = {
 			discordId: messageAuthorId,
@@ -43,62 +42,64 @@ export function ingestTradingMessage(messageContent: string, messageAuthorId: st
 
 		for (let word of line.split(/\s+/)) {
 			word = word.toLowerCase();
-			console.log(`at word: ${word}, currentParam ${currentParam}, sellingFlag ${sellingFlag}`);
-			if (currentParam == "") {
-				if (word.startsWith("sell")) {
-					sellingFlag = true;
-					buyingFlag = false;
-					listing.buying = false;
-				}
-				else if (word.startsWith("buy")) {
-					sellingFlag = false;
-					buyingFlag = true;
-					listing.buying = true;
-				}
-				else if (word.endsWith("x") && word != "phoenix") {
-					listing.item.count = parseInt(word.split("x")[0]);
-				}
-				else if (word == "for" || word == "bin") {
-					currentParam = "forPrice";
-				}
-				else {
-					let resolvedEnchantName = resolveEnchantAlias(stripNumbersFromEnd(word));
-					if (resolvedEnchantName) {
-						let enchantLevel = 0;
-						let parsedEnchantLevel = getNumbersAtEndOfString(word);
-						if (parsedEnchantLevel) {
-							enchantLevel = parsedEnchantLevel;
-						}
-						let parsedEnchant = {
-							key: resolvedEnchantName,
-							level: enchantLevel,
-						} as Enchant;
-						if (listing.item.enchants == null) {
-							listing.item.enchants = [];
-						}
-						listing.item.enchants.push(parsedEnchant);
-						console.log(`resolved enchant successfully as ${resolvedEnchantName} ${enchantLevel}`);
+			console.log(`at word: ${word}, sellingFlag ${sellingFlag}, buyingFlag ${buyingFlag}, parsedAnyInfoAboutItem ${parsedAnyInfoAboutItem}`);
+			if (word.startsWith("sell")) {
+				sellingFlag = true;
+				buyingFlag = false;
+				listing.buying = false;
+			}
+			else if (word.startsWith("buy")) {
+				sellingFlag = false;
+				buyingFlag = true;
+				listing.buying = true;
+			}
+			else if (word.endsWith("x") && word != "phoenix") {
+				listing.item.count = parseInt(word.split("x")[0]);
+			}
+			else if (word == "for" || word == "bin") {
+				let priceStr = line.split(word)[1];
+				console.log(`got price string: ${priceStr}`);
+				let separatedPriceInfo = separatePriceInfo(priceStr.replaceAll(" ", ""));
+				console.log(`got price info: ${JSON.stringify(separatedPriceInfo)}`);
+				if (separatedPriceInfo) {
+					listing.pricePer = separatedPriceInfo.numbers;
+					let attemptMatchCurrency = matchCurrency(separatedPriceInfo.letters);
+					if (attemptMatchCurrency) {
+						listing.currency = attemptMatchCurrency;
+						console.log(`matched currency successfully as ${attemptMatchCurrency}, breaking`);
+						break;
+					}
+					else {
+						console.log("failed to match currency");
 					}
 				}
 			}
-			else if (currentParam == "forPrice") {
-				listing.pricePer = parseInt(word);
-				currentParam = "currencyName";
-			}
-			else if (currentParam == "currencyName") {
-				let attemptMatchCurrency = matchCurrency(word);
-				if (attemptMatchCurrency) {
-					listing.currency = attemptMatchCurrency;
-					console.log(`matched currency successfully as ${attemptMatchCurrency}, breaking`);
-					break;
+			else {
+				let resolvedEnchantName = resolveEnchantAlias(stripNumbersFromEnd(word));
+				if (resolvedEnchantName) {
+					let enchantLevel = 0;
+					let parsedEnchantLevel = getNumbersAtEndOfString(word);
+					if (parsedEnchantLevel) {
+						enchantLevel = parsedEnchantLevel;
+					}
+					let parsedEnchant = {
+						key: resolvedEnchantName,
+						level: enchantLevel,
+					} as Enchant;
+					if (listing.item.enchants == null) {
+						listing.item.enchants = [];
+					}
+					listing.item.enchants.push(parsedEnchant);
+					parsedAnyInfoAboutItem = true;
+					console.log(`resolved enchant successfully as ${resolvedEnchantName} ${enchantLevel}`);
 				}
 			}
 		}
 
-		if (sellingFlag || buyingFlag) {
+		if (parsedAnyInfoAboutItem && (sellingFlag || buyingFlag)) {
 			console.log("listing:");
 			console.log(JSON.stringify(listing, null, 4));
-			if (collections.listings) {
+			if (collections.listings && process.env.node_env == "production") {
 				collections.listings.insertOne(listing);
 				sendDiscordMessage("log-newlistings", "new listing from discord:\n```json\n" + JSON.stringify(listing, null, 4) + "```");
 			}
@@ -117,8 +118,32 @@ function getNumbersAtEndOfString(input: string): number | null {
 
 function matchCurrency(input: string): Currency | undefined {
 	const normalizedInput = input.toLowerCase();
-	const matchedKey = Object.keys(Currency).find(key => 
+
+	let matchedKey = Object.keys(Currency).find(key => 
 		Currency[key as keyof typeof Currency].toLowerCase() === normalizedInput
 	);
+
+	if (!matchedKey && normalizedInput.endsWith('s')) {
+		const inputWithoutS = normalizedInput.slice(0, -1);
+		matchedKey = Object.keys(Currency).find(key => 
+			Currency[key as keyof typeof Currency].toLowerCase() === inputWithoutS
+		);
+	}
+
 	return matchedKey ? Currency[matchedKey as keyof typeof Currency] : undefined;
+}
+
+function separatePriceInfo(input: string): { letters: string, numbers: number } | false {
+	if (!/^([a-zA-Z]+[0-9]+|[0-9]+[a-zA-Z]+)$/.test(input)) {
+		return false;
+	}
+
+	const lettersMatch = input.match(/[a-zA-Z]+/);
+	const numbersMatch = input.match(/[0-9]+/);
+
+	if (lettersMatch && numbersMatch) {
+		return { letters: lettersMatch[0], numbers: parseInt(numbersMatch[0]) };
+	}
+
+	return false;
 }
